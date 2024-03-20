95633446e33e352aea60a2d233e1523f55ac5a88
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-03-20 11:43:21 +0900

    1929번
    에라토스테네스의 체

---
+++ src/main/java/org/example/Q1929.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q1929 {
+    public static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        String[] list = str.split(" ");
+        prime = new boolean[Integer.parseInt(list[1]) + 1];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+        for (int i = 2; i*i <= Integer.parseInt(list[1]); i++) {
+            if (prime[i]) {
+                for (int j = i*i; j <= Integer.parseInt(list[1]); j+=i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        for (int i = Integer.parseInt(list[0]); i <= Integer.parseInt(list[1]); i++) {
+            if (prime[i]) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
