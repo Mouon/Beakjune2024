9b222cb9c26c1b42f1c8e2942dba31c71970c1c6
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-03-25 17:58:27 +0900

    2133
    DP 고난이도 문제.. 진짜 따질 것이 많다..대박이다..
    
    예외 경우 처리하고 규칙잡기 DP메모리제이션시 중복경우 제외하는 법까지
    
    복습이 필요한 문제이다.

---
+++ src/main/java/org/example/Q2133.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2133 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp=new long[31];
+
+        dp[0]=1;
+        dp[2]=3;
+        dp[4]=11;
+
+        for(int i=6;i<31;i+=2){
+            dp[i] = dp[i - 2]*3;
+            for(int j=0;j<i-2;j+=2){
+                dp[i]+=dp[j]*2;
+            }
+        }
+
+        System.out.print(dp[n]);
+
+    }
+
+    /**
+     * 3*6=41
+     * */
+}
+
