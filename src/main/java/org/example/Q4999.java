35e71e51b649e5cfddf8a05e1c89c6d2abd16ce7
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-03-25 18:12:05 +0900

    4999
    몸 풀기

---
+++ src/main/java/org/example/Q4999.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Q4999 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String N=scan.nextLine();;
+        String K=scan.nextLine();;
+
+        System.out.println(N.length()<K.length()?"no":"go");
+
+    }
+
+}
