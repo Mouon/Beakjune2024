42432a1021dfd1a78b06fecdd83adc314f8d3dd7
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-03-25 09:56:11 +0900

    4101
    연습용

---
+++ src/main/java/org/example/Q18108.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Q18108 {
+
+    static public void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+        System.out.println(year-543);
+    }
+
+}
+
---
+++ src/main/java/org/example/Q4101.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Q4101 {
+    public static void main(String[] args)  throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] list = new String[1000000];
+        int i=0;
+        while (true) {
+            String input = br.readLine();
+            if(input.equals("0 0")){
+                break;
+            }
+            list[i] = input;
+            i++;
+        }
+        for (int j=0;j<i;j++){
+            String[] arr;
+            arr=list[j].split(" ");
+            System.out.println(Integer.parseInt(arr[0])>Integer.parseInt(arr[1])?"Yes":"No");
+        }
+
+    }
+}
