981e8bcd3caf2e112438feecbddd1d8a3dbb836f
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-05-19 17:37:24 +0900

    머리풀기

---
+++ src/main/java/org/example/math/Q2501.java
@@ -0,0 +1,30 @@
+package org.example.math;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q2501 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        String[] strNum = str.split(" ");
+
+
+        int N = Integer.parseInt(strNum[0]);
+        int K = Integer.parseInt(strNum[1]);
+        int[] M = new int[N+1];
+
+
+        for(int i=1;i<=N;i++){
+            if(N%i==0){
+                M[i]=i;
+            }
+        }
+
+        System.out.println(M[K]);
+
+
+    }
+}
