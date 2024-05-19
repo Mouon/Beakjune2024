f772dbeecc23583d2370df7b6592a304c27b4e5f
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-05-19 17:52:54 +0900

    그냥 갯수채우기용

---
+++ src/main/java/org/example/implement/Q2741.java
@@ -0,0 +1,17 @@
+package org.example.implement;
+
+import java.util.Scanner;
+
+public class Q2741 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for(int i=1;i<=N;i++){
+            System.out.println(i);
+        }
+
+    }
+}
