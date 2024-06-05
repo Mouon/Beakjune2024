4dd7a67711b4a2308813498f5e237689999050ef
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-06-06 00:35:30 +0900

    2차원배열 정렬 : 블로그 복습 (내 블로그 글)
    인터페이스 오버라이딩해서 쓰면되는데 원리는 2차원배열 -> 일차원 배열

---
+++ src/main/java/org/example/implement/Q11651.java
@@ -0,0 +1,41 @@
+package org.example.implement;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Q11651 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        scan.nextLine();
+        int[][] cord = new int[N][2];/** 멍청하게 N 이라는 변수로 둬야하는데 5라고둬서 틀림 */
+
+        for(int i=0;i<N;i++){
+            String str = scan.nextLine();
+            String[] arr = str.split(" ");
+            cord[i][0]=Integer.parseInt(arr[0]);
+            cord[i][1]=Integer.parseInt(arr[1]);
+        }
+
+        Arrays.sort(cord, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                int yCompare = Integer.compare(o1[1],o2[1]);
+                if(yCompare==0){
+                    return Integer.compare(o1[0],o2[0]);
+                }
+                return yCompare;
+            }
+        });
+
+        for(int i=0;i<N;i++){
+            System.out.print(cord[i][0]+" ");
+            System.out.println(cord[i][1]);
+
+        }
+
+    }
+}
