fd9a630852de866bd805f4ebd3b4abc2ca20810f
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-06-07 10:00:06 +0900

    2차원배열 정렬 : 블로그 복습 (내 블로그 글)
    인터페이스 오버라이딩해서 쓰면되는데 원리는 2차원배열 -> 일차원 배열

--- src/main/java/org/example/implement/Q11651.java
+++ src/main/java/org/example/implement/Q11651.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Q11651 {
     public static void main(String[] args) throws Exception {
 
+
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
         scan.nextLine();
