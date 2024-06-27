957e1041a70ae1b9ab01facd239f98432a00f0f2
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-06-27 22:55:43 +0900

    1932 정수 삼각형 : 다시풀기
    dp buttom up 머시기 공부할 점이 분명존재하는 문제이다.

---
+++ src/main/java/org/example/dp/Q1932.java
@@ -0,0 +1,29 @@
+package org.example.dp;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q1932 {
+    static int dp[][];
+    public static void main(String[] args) throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        dp=new int[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<=i;j++){
+                dp[i][j]=scan.nextInt();
+            }
+        }
+
+        for(int i=N-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                dp[i][j]+=Math.max(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+
+        System.out.println(dp[0][0]);
+
+    }
+
+}
