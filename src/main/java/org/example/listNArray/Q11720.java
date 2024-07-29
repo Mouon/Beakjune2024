6cc1e5bec7ca7c3a28e0ef3620dd22e451d7601c
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-07-29 20:13:07 +0900

    11720 숫자합 구하기 : 아스키 코드 사용

--- src/main/java/org/example/listNArray/Q11720.java
+++ src/main/java/org/example/listNArray/Q11720.java
@@ -1,7 +1,9 @@
 package org.example.listNArray;
 
 import java.util.Scanner;
-
+/**
+ * 아스키 성질 이용
+ * */
 public class Q11720 {
     public static void main(String[] args) throws Exception {
 
@@ -10,13 +12,25 @@ public class Q11720 {
         int sum =0;
         scan.nextLine();
         String str = scan.nextLine();
-        String strArr[] = str.split("");
-        for (int i =0;i<strArr.length;i++){
-                sum+=Integer.parseInt(strArr[i]);
+        char chArr[] = str.toCharArray();
+        for (int i =0;i<chArr.length;i++){
+            sum+=chArr[i] - '0';
         }
         System.out.println(sum);
-
-
     }
 
 }
+
+/**
+ *         Scanner scan = new Scanner(System.in);
+ *         int N = scan.nextInt();
+ *         int sum =0;
+ *         scan.nextLine();
+ *         String str = scan.nextLine();
+ *         String strArr[] = str.split("");
+ *         for (int i =0;i<strArr.length;i++){
+ *                 sum+=Integer.parseInt(strArr[i]);
+ *         }
+ *         System.out.println(sum);
+ *
+ * */
