d883d823819fcf8339941696bb1f01d8500b5afd
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-08-02 17:04:21 +0900

    Q9663 : N 퀸 문제 시작

---
+++ src/main/java/org/example/implement/Q9663.java
@@ -0,0 +1,14 @@
+package org.example.implement;
+
+import java.io.IOException;
+
+/**
+ * N 퀸
+ * */
+public class Q9663 {
+    public static void main(String[] args) throws IOException {
+
+
+
+    }
+}
