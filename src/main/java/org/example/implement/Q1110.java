ddea4c3ae6d99121a250f1f2a33231aa93815fff
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-08-07 16:47:50 +0900

    Q1764 : 듣보잡
    getOrDefault 공부함

---
+++ src/main/java/org/example/implement/Q1110.java
@@ -0,0 +1,29 @@
+package org.example.implement;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q1110 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int K = N;
+        int count =0;
+        while (true){
+
+            int i = N/10;
+            int j =N%10;
+            N=j*10+((i+j)%10);
+            count++;
+//            System.out.println(N);
+            if(N==K){
+                break;
+            }
+
+        }
+        
+        System.out.println(count);
+    }
+}
--- src/main/java/org/example/implement/Q1764.java
+++ src/main/java/org/example/implement/Q1764.java
@@ -1,29 +1,38 @@
 package org.example.implement;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Q1764 {
     public static void main(String[] args) throws IOException {
 
-        Scanner scan = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = scan.nextInt();
-        int K = N;
-        int count =0;
-        while (true){
 
-            int i = N/10;
-            int j =N%10;
-            N=j*10+((i+j)%10);
-            count++;
-//            System.out.println(N);
-            if(N==K){
-                break;
-            }
-
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Map<String, Integer> unListen = new HashMap<>();
+        ArrayList<String> results = new ArrayList<>();
+        for(int i=0;i<N;i++){
+            unListen.put(br.readLine(),1);
         }
-        
-        System.out.println(count);
+
+        for(int i=0;i<M;i++){
+            String temp = br.readLine();
+            unListen.put(temp,unListen.getOrDefault(temp,0)+1);
+            if(unListen.get(temp)==2) results.add(temp);
+        }
+
+
+        Collections.sort(results);
+
+        System.out.println(results.size());
+        for(int i=0;i< results.size();i++){
+            System.out.println(results.get(i));
+        }
+
     }
 }
