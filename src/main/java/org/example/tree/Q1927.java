296b22aea4f7f150c7013aa4f698658373c48d16
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-08-12 00:00:19 +0900

    Q1927 : 최소 힙
    그니까 그냥 최솟값 출력

---
+++ src/main/java/org/example/tree/Q1927.java
@@ -0,0 +1,34 @@
+package org.example.tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Q1927 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> minQ = new PriorityQueue<>();
+
+        for(int i=0;i<N;i++){
+            int input=Integer.parseInt(br.readLine());
+
+            if(input>0){
+                minQ.add(input);
+            }else {
+                if(!minQ.isEmpty()){
+                    System.out.println(minQ.poll());
+                }else {
+                    System.out.println(0);
+                }
+            }
+        }
+
+
+    }
+
+}
