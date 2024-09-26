1ba2aa7076b11c73742675609e333ea162374182
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-09-26 12:56:52 +0900

    토스 기출 : 멋쟁이 개선 풀이

--- src/main/java/org/example/dfsNbfs/Toss2022.java
+++ src/main/java/org/example/dfsNbfs/Toss2022.java
@@ -13,21 +13,37 @@ public class Toss2022 {
         String arr[] = input.split("");
         int sol=-1;
         for (int i=0;i< arr.length-2;i++){
-
-            if(Integer.parseInt(arr[i])==Integer.parseInt(arr[i+1])&&Integer.parseInt(arr[i+1])==
-                    Integer.parseInt(arr[i+2])&&Integer.parseInt(arr[i])>sol){
-                sol=Integer.parseInt(arr[i]);
+            if(Integer.parseInt(arr[i])==Integer.parseInt(arr[i+1])&&Integer.parseInt(arr[i+1])==Integer.parseInt(arr[i+2])){
+                sol=Math.max(Integer.parseInt(input.substring(i,i+3)),sol);
             }
         }
 
-        if(sol==-1){
-            System.out.print(sol);
-            return;
-        }
         System.out.print(sol);
-        System.out.print(sol);
-        System.out.print(sol);
-
     }
 
 }
+
+/**  @초기풀이
+ *
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         String input = br.readLine();
+ *         String arr[] = input.split("");
+ *         int sol=-1;
+ *         for (int i=0;i< arr.length-2;i++){
+ *
+ *             if(Integer.parseInt(arr[i])==Integer.parseInt(arr[i+1])&&Integer.parseInt(arr[i+1])==
+ *                     Integer.parseInt(arr[i+2])){
+ *                 sol=Integer.parseInt(arr[i]);
+ *             }
+ *         }
+ *
+ *         if(sol==-1){
+ *             System.out.print(sol);
+ *             return;
+ *         }
+ *         System.out.print(sol);
+ *         System.out.print(sol);
+ *         System.out.print(sol);
+ *
+ *
+ * */
