91646db21c4bb7bf7fc34433d287af55fedf732e
Mouon <ahemsapsldk@gmail.com>
2025-01-13 20:03:02 +0900

    feat : 1629

--- src/main/java/org/example/math/Q1629.java
+++ src/main/java/org/example/math/Q1629.java
@@ -3,6 +3,10 @@ package org.example.math;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+/** (a^(b/2) × a^(b/2)) mod C
+ * (a^(b/2) × a^(b/2+1)) mod C
+ */
+
 
 public class Q1629 {
     public static void main(String[] args) throws Exception {
@@ -18,6 +22,7 @@ public class Q1629 {
 
     }
 
+    // 플래그 포인트는 0, 1 이다.
     public static long Cur (long base, long exponent, long mod){
         if(exponent == 0) {
             return 1;
@@ -25,14 +30,11 @@ public class Q1629 {
         if(exponent == 1) {
             return base%mod;
         }
-        long temp = Cur(base,exponent/2,mod);
+        long temp=Cur(base,exponent/2,mod);
         if(exponent%2==0){
-            return temp * temp % mod;
-        }else{
-            return temp * temp % mod*base%mod;
+            return (temp*temp)%mod;
+        }else {
+            return (temp*Cur(base,exponent/2+1,mod))%mod;
         }
-
     }
-
-
 }
