7572b83bdb9ecf85a5fbbda79f125bf6e59f7e40
Mouon <ahemsapsldk@gmail.com>
2025-01-27 00:39:45 +0900

    feat : 2156
    dp 오랜만에 풀었더니 감잃음

---
+++ src/main/java/org/example/dp/Q2156.java
@@ -0,0 +1,37 @@
+package org.example.dp;
+
+import java.util.Scanner;
+
+public class Q2156 {
+    static int[] dp;
+    static int[] wines;
+    static int n;
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        n = scan.nextInt();
+
+        wines = new int[n+1];
+        dp = new int[n+1];
+
+        for(int i=1;i<n+1;i++){
+            wines[i] = scan.nextInt();
+        }
+
+        dp[1] = wines[1];
+
+        if(n > 1) {
+            dp[2] = wines[1] + wines[2];
+        }
+
+        for(int i=3;i<n+1;i++){
+            dp[i] = Math.max(dp[i-1],Math.max(dp[i-2],dp[i-3]+wines[i-1])+wines[i]);
+        }
+
+        System.out.println(dp[n]);
+
+    }
+
+}
