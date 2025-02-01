46024946f2fdc95f2734cf377224f957fe5b98dd
Mouon <ahemsapsldk@gmail.com>
2025-02-01 16:19:20 +0900

    feat : 2178
    탐색이다!!

--- src/main/java/org/example/dfsNbfs/Q2178.java
+++ src/main/java/org/example/dfsNbfs/Q2178.java
@@ -10,8 +10,9 @@ public class Q2178  {
         static int[] dy = {1,0,-1,0};
 
         static int N,M;
-        static int[][] A;
+        static int[][] miro;
         static boolean[][] visited;
+
 
     public static void main(String[] arg) throws IOException {
 
@@ -19,39 +20,67 @@ public class Q2178  {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        A =new int[N][M];
+        miro =new int[N][M];
         visited = new boolean[N][M];
 
+        /**
+         * 미로 채우기
+         * */
         for (int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
             String line = st.nextToken();
           for (int j=0;j<M;j++){
-              A[i][j]=Integer.parseInt(line.substring(j,j+1));
+              miro[i][j] = Integer.parseInt(line.substring(j,j+1));
           }
         }
-        BFS(0,0);
-        System.out.println(A[N-1][M-1]);
+
+        search(0,0);
+        System.out.println(miro[N-1][M-1]);
+
+
     }
 
-    private static void BFS(int i, int j) {
-
+    static void search(int i, int j){
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[] {i,j});
         while (!queue.isEmpty()){
+
             int now[] = queue.poll();
-            visited[i][j]=true;
-            for (int k=0; k<4; k++){
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
+            visited[i][j] = true;
+
+            for(int l=0;l<4;l++) {
+
+                int x = now[0] + dx[l];
+                int y = now[1] + dy[l];
+
                 if(x>=0 && y>=0 && x<N && y<M){
-                    if(A[x][y]!=0&&visited[x][y]!=true){
-                       visited[x][y] = true;
-                       A[x][y] = A[now[0]][now[1]] + 1;
-                       queue.add(new int[] {x,y});
+                    if(miro[x][y]!=0&&!visited[x][y]){
+                        visited[x][y] = true;
+                        miro[x][y] += miro[now[0]][now[1]];
+                        queue.add(new int[]{x, y});
+                    }
+                }
+            }
+        }
+    }
+
+    static void search2 (int i, int j){
+
+        visited[i][j] = true;
+
+            for(int l=0;l<4;l++) {
+
+                int x = i + dx[l];
+                int y = j + dy[l];
+
+                if(x>=0 && y>=0 && x<N && y<M){
+                    if(miro[x][y]!=0&&!visited[x][y]){
+                        visited[x][y] = true;
+                        miro[x][y] += miro[i][j];
+                        search2(x,y);
                     }
                 }
             }
         }
 
-    }
 }
