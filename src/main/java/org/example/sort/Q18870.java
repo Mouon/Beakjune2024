a9c1c145f2c6936e0c7cec482a5f80bcf79731d9
Mouon <ahemsapsldk@gmail.com>
2025-03-14 17:36:42 +0900

    feat : 정렬 연습 시작 2일차

--- src/main/java/org/example/sort/Q18870.java
+++ src/main/java/org/example/sort/Q18870.java
@@ -1,7 +1,43 @@
 package org.example.sort;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Q18870 {
+    static int[] arr,temp;
+    static HashMap<Integer,Integer> map;
+    static int N;
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        N = scan.nextInt();
+
+        arr = new int[N];
+        temp = new int[N];
+        map = new HashMap<>();
+
+        for(int i=0;i<N;i++){
+            arr[i] =  scan.nextInt();
+            temp[i] = arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        int rank = 0;
+
+        for(int i : arr){
+            if(!map.containsKey(i)){
+                map.put(i,rank++);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int k : temp){
+            sb.append(map.get(k)).append(" ");
+        }
+
+        System.out.print(sb);
 
     }
 }
