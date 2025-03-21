87e1695f1d96a38b08a8c9dfdd85c6a94f69437e
Mouon <ahemsapsldk@gmail.com>
2025-03-21 16:44:43 +0900

    feat : 프로그래머스 연습중

--- src/main/java/org/example/Q3190.java
+++ src/main/java/org/example/Q3190.java
@@ -5,113 +5,97 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/**
- * 뱀의 꼬리 짜른다 -> 선입선출
- *
- * 뱀을 큐로 구현했다가 수정
- * */
-
 public class Q3190 {
 
     static int[][] board;
-    static int N, K, L;
-
-    /** 방향순서 난관 오른쪽 기준 모호
-     * 오른쪽, 아래쪽, 왼쪽, 위쪽
-     * 배열의 [][]랑 (,)이거랑 헷갈려서 헤맴
-     * */
+    static int N, K, L,T=0;
     static int[] dx = { 0, 1, 0, -1 };
     static int[] dy = { 1, 0, -1, 0 };
+
 
     static HashMap<Integer,String> turnSignal = new HashMap<>();
     static ArrayList<int[]> snake= new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
+        Scanner scan =  new Scanner(System.in);
+        N = scan.nextInt();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        K = Integer.parseInt(br.readLine());
+        board = new int[N][N];
 
-        board=new int[N][N];
+        K = scan.nextInt();
 
         for(int i=0;i<K;i++){
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            board[x-1][y-1]=1;
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+
+            board[x-1][y-1] = 1;
         }
 
-        L = Integer.parseInt(br.readLine());
+        L = scan.nextInt();
+        scan.nextLine();
 
         for(int i=0;i<L;i++){
-            st = new StringTokenizer(br.readLine());
-            int sec = Integer.parseInt(st.nextToken());
-            String direction = st.nextToken();
-            turnSignal.put(sec,direction);
+            String str = scan.nextLine();
+            String[] line = str.split(" ");
+            int sec = Integer.parseInt(line[0]);
+            String dic = line[1];
+
+            turnSignal.put(sec,dic);
         }
 
-        play();
+        bfs(0,0);
 
+        System.out.print(T);
 
     }
 
-
-    static void play(){
-        int time = 0;
-        int currentX = 0;
-        int currentY = 0;
+    static public void bfs(int startX,int startY){
+        snake.add(new int[]{startX,startY});
         int direction = 0;
-        snake.add(new int[] { 0, 0 });
 
         while (true){
+            T++;
+            int x = startX+dx[direction];
+            int y = startY+dy[direction];
 
-            time++;
-            int nowX = currentX+dx[direction];
-            int nowY = currentY+dy[direction];
 
-
-            if(nowX<0 || nowY <0 || nowX > N-1 || nowY > N-1){
+            if(x<0 || y<0 || x > N-1 || y > N-1){
                 break;
             }
 
-            if(validateSnake(nowX,nowY)){
+            if(isSnake(x,y)){
                 break;
             }
 
-            if(board[nowX][nowY] == 1){
-                snake.add(new int[] {nowX,nowY});
-                board[nowX][nowY] = 0;
+            if(board[x][y] == 1){
+                snake.add(new int[] {x,y});
+                board[x][y] = 0;
             } else {
-                snake.add(new int[] {nowX,nowY});
+                snake.add(new int[] {x,y});
                 snake.remove(0);
             }
 
-            if(turnSignal.containsKey(time)){
-                if(turnSignal.get(time).equals("L")){
+            if(turnSignal.containsKey(T)){
+                if(turnSignal.get(T).equals("L")){
                     direction = (direction + 3) % 4;
                 }else {
                     direction = (direction + 1) % 4;
                 }
             }
 
-            currentX = nowX;
-            currentY = nowY;
+            startX = x;
+            startY =y;
 
         }
-
-        System.out.println(time);
-
     }
 
-    static boolean validateSnake(int nowX,int nowY){
-
-        for(int i=0;i<snake.size()-1;i++){
-            int[] temp = snake.get(i);
-            if(nowX==temp[0]&&nowY==temp[1]){
+    static boolean isSnake(int x,int y){
+        for (int[] temp : snake) {
+            if (x == temp[0] && y == temp[1]) {
                 return true;
             }
         }
         return false;
     }
+
 }
