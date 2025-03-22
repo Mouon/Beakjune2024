199d18c60393068810318eaed3fa992d48320efc
Mouon <ahemsapsldk@gmail.com>
2025-03-22 19:17:58 +0900

    feat : dp 복습중

--- src/main/java/org/example/dp/Q11660.java
+++ src/main/java/org/example/dp/Q11660.java
@@ -4,42 +4,40 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Q11660 {
-
-    static int dp[][];
+    static int N;
+    static int M;
+    static int[][] board;
     public static void main(String[] args) throws IOException {
 
         Scanner scan = new Scanner(System.in);
+        N = scan.nextInt();
+        M = scan.nextInt();
 
-        int N = scan.nextInt();
-        int num = scan.nextInt();
-        int[] sol = new int[num];
-        dp=new int[N+1][N+1];
-        int[][] matrix = new int[N + 1][N + 1];
+        int[][] board = new int[N + 1][N + 1];
+        int[][] dp = new int[N + 1][N + 1];
 
-        for(int i=1;i<N+1;i++){
-            for(int j=1;j<N+1;j++){
-                matrix[i][j]=scan.nextInt();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                board[i][j] = scan.nextInt();
             }
         }
 
-        for(int i=1;i<N+1;i++){
-            for(int j=1;j<N+1;j++){
-                dp[i][j] = dp[i-1][j]+dp[i][j-1]+matrix[i][j]-dp[i-1][j-1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + board[i][j];
             }
         }
 
 
-        for(int i=0;i<num;i++){
-            int x=scan.nextInt();
-            int y=scan.nextInt();
-            int z=scan.nextInt();
-            int w=scan.nextInt();
+        for (int i = 0; i < M; i++) {
+            int x1 = scan.nextInt();
+            int y1 = scan.nextInt();
+            int x2 = scan.nextInt();
+            int y2 = scan.nextInt();
 
-            sol[i]=dp[z][w]-dp[x-1][w]-dp[z][y-1]+dp[x-1][y-1];
+            int sum = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+            System.out.println(sum);
+        }
 
-        }
-        for(int i=0;i<num;i++){
-            System.out.println(sol[i]);
-        }
     }
 }
