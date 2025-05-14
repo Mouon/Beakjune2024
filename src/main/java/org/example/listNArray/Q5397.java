fcb8c512501770d65cd39fda4b52f2962e3af8d5
Mouon <ahemsapsldk@gmail.com>
2025-05-14 14:44:24 +0900

    feat : 5397 작전상 후퇴

---
+++ src/main/java/org/example/listNArray/Q5397.java
@@ -0,0 +1,51 @@
+package org.example.listNArray;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Q5397 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int count = scan.nextInt();
+        scan.nextLine();
+        for(int i=0;i<count;i++){
+            String input = scan.nextLine();
+            System.out.println(findPw(input));
+        }
+
+    }
+
+    static String findPw(String input){
+        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack2 = new Stack<>();
+        String ans ="";
+        for(int i=0;i<input.length();i++){
+            if(input.charAt(i)=='<'&&!stack.isEmpty()){
+                stack2.add(stack.pop());
+            } else if (input.charAt(i)=='>'&&!stack2.isEmpty()) {
+                stack.add(stack2.pop());
+            } else if (input.charAt(i)=='-'&&!stack.isEmpty()) {
+                stack.pop();
+            } else if(input.charAt(i)!='<'&&input.charAt(i)!='>'){
+                stack.add(input.charAt(i));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int L = stack.size();
+        String temp = "";
+        for(int i=0;i<L;i++){
+            temp = String.valueOf(sb.append(stack.pop()));
+        }
+
+        for(int i=L-1;i>=0;i--){
+            ans = ans + temp.charAt(i);
+        }
+
+
+        return ans;
+
+    }
+}
---
+++ src/main/java/org/example/math/Q2166.java
@@ -0,0 +1,31 @@
+package org.example.math;
+
+import java.util.Scanner;
+
+// 왜 틀리누
+public class Q2166 {
+    static int N ;
+    static int[][] index;
+    public static void main(String[] args) {
+        int a=0,b=0;
+        Scanner scan = new Scanner(System.in);
+        N = scan.nextInt();
+        index = new int[N][2];
+
+        for (int i=0;i<N;i++){
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            index[i][0] = x;
+            index[i][1] = y;
+        }
+
+        for (int i=0;i<N-1;i++){
+            a += index[i][0] * index[i+1][1];
+            b += index[i][1] * index[i+1][0];
+        }
+
+        double area = Math.abs(a - b) / 2.0;
+        System.out.printf("%.1f", area);
+
+    }
+}
