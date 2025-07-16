f68164404ae7b7d22448823a256a88ff2d4cc105
Mouon <ahemsapsldk@gmail.com>
2025-07-16 09:18:47 +0900

    feat : 미친 long 이잖아

---
+++ src/main/java/org/example/dp/Q2748.java
@@ -0,0 +1,22 @@
+package org.example.dp;
+
+import java.util.Scanner;
+
+public class Q2748 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        long[] dp = new long[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
