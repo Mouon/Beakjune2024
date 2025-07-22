c55a7fb04caf5d18048a810be09d819b39d54c10
Mouon <ahemsapsldk@gmail.com>
2025-07-22 18:16:52 +0900

    feat : 오큰수
    스택의 특성을 이렇게 활용하다니 감탄만 나오는 풀이다

--- src/main/java/org/example/stackQ/Q17298.java
+++ src/main/java/org/example/stackQ/Q17298.java
@@ -5,35 +5,39 @@ import java.util.Stack;
 
 /**
  * 블로그 볼거임 진짜 모르겠음
+ *
+ * 인덱스와 스택을 이용한 아주 고능한 문제풀이
  * */
 public class Q17298 {
     static int N;
-    static int[] arr , ans;
+    static int[] arr;
     static Stack<Integer> stack = new Stack<>();
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         N = scan.nextInt();
 
         arr = new int[N];
-        ans = new int[N];
 
         for(int i=0;i<N;i++){
             arr[i] = scan.nextInt();
-            ans[i] = -1;
         }
 
         for(int i=0;i<N;i++){
-            for(int j=i;j<N;j++){
-                if(arr[i] < arr[j]){
-                    ans[i] = arr[j];
-                    break;
-                }
-            }
+           while (!stack.isEmpty() && (arr[stack.peek()] < arr[i])){
+               arr[stack.pop()] = arr[i];
+           }
+           stack.push(i);
         }
 
-        for (int num : ans){
-            System.out.print(num + " ");
+        while (!stack.isEmpty()){
+            arr[stack.pop()] = -1;
         }
 
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++){
+            sb.append(arr[i]).append(' ');
+        }
+
+        System.out.println(sb);
     }
 }
