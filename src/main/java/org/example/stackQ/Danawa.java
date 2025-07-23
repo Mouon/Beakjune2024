c47afd14fb61404740cca7d90e2176626affd889
Mouon <ahemsapsldk@gmail.com>
2025-07-24 08:48:12 +0900

    feat : 큐 판단

--- src/main/java/org/example/stackQ/Danawa.java
+++ src/main/java/org/example/stackQ/Danawa.java
@@ -11,16 +11,34 @@ public class Danawa {
         int n = arr.length;
         int current = 1;
         for (int target : arr) {
-            while (current <= n && (stack.isEmpty() || stack.peek() < target)) {
+            while (current<n && (stack.isEmpty()||stack.peek()<target)){
                 stack.push(current);
                 current++;
             }
-            if (!stack.isEmpty() && stack.peek() == target) {
+            if(!stack.isEmpty() && stack.peek()==target){
                 stack.pop();
-            } else {
+            }else {
                 return false;
             }
         }
         return true;
     }
 }
+
+//public boolean solution (int[] arr) {
+//    Stack<Integer> stack = new Stack<>();
+//    int n = arr.length;
+//    int current = 1;
+//    for (int target : arr) {
+//        while (current <=n && (stack.isEmpty() || stack.peek() < target)){
+//            stack.push(current);
+//            current++;
+//        }
+//        if (!stack.isEmpty() && stack.peek()==target){
+//            stack.pop();
+//        }else {
+//            return false;
+//        }
+//    }
+//    return true;
+//}
