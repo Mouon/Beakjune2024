d9768b782c23e7976e01f7cb56f66fdac82628e3
Mouon <ahemsapsldk@gmail.com>
2025-08-06 12:36:41 +0900

    feat : dp는 그냥 어렵구나

--- src/main/java/org/example/dp/Q11057.java
+++ src/main/java/org/example/dp/Q11057.java
@@ -1,28 +1,41 @@
 package org.example.dp;
 
-public class Q11057 {
-    public static void main(String[] args) {
-        System.out.println(solution("123"));
-    }
-    static public int solution(String s){
-        int ans = -1;
-        int count = 1;
+import java.util.Scanner;
 
-        char before = s.charAt(0);
-        for(int i =1;i<s.length()-1;i++){
-            if(before==s.charAt(i)){
-                count++;
-            }else {
-                count=1;
-            }
-            if(count==3){
-                count=1;
-                if(ans<before){
-                    ans = Character.getNumericValue(before);
+/**
+ * 블로그 볼거임
+ * */
+public class Q11057 {
+
+    static int dp[][];
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        int ans = 0;
+
+        dp = new int[N+1][10];
+
+        for(int i=0;i<10;i++){
+            dp[1][i] = 1;
+        }
+
+        for(int i=2;i<=N;i++){
+            for(int j=0;j<10;j++){
+                for (int k=0;k<=j;k++){
+                    dp[i][j] += dp[i-1][k];
+                    dp[i][j] %= 10007;
                 }
             }
-            before = s.charAt(i);
         }
-        return ans;
+
+        for(int i=0;i<10;i++){
+            ans += dp[N][i];
+        }
+
+        System.out.println(ans % 10007);
+
     }
 }
