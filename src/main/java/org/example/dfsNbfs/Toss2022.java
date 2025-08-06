504d26d7dcbffb43e61ec0397c00c7c69acb2421
Mouon <ahemsapsldk@gmail.com>
2025-08-06 10:01:45 +0900

    feat : toss 2022

--- src/main/java/org/example/dfsNbfs/Toss2022.java
+++ src/main/java/org/example/dfsNbfs/Toss2022.java
@@ -21,6 +21,28 @@ public class Toss2022 {
         System.out.print(sol);
     }
 
+    static public int solution(String s){
+        int ans = -1;
+        int count = 1;
+
+        char before = s.charAt(0);
+        for(int i =1;i<s.length()-1;i++){
+            if(before==s.charAt(i)){
+                count++;
+            }else {
+                count=1;
+            }
+            if(count==3){
+                count=1;
+                if(ans<before){
+                    ans = Character.getNumericValue(before);
+                }
+            }
+            before = s.charAt(i);
+        }
+        return ans;
+    }
+
 }
 
 /**  @초기풀이
